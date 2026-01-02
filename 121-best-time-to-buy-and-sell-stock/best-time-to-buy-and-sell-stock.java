c18d4857fc47b9f5ad3dfd3a8c11f89cb1da7f34
PRIYA SAMMAL <152009465+priya-sammal@users.noreply.github.com>
2026-01-02 16:06:16 +0530

    Time: 2 ms (42.24%) | Memory: 94.8 MB (12.98%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int minPrice = prices[0]; // sabse sasta buy
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i]; // better buy day
+            } else {
+                int profit = prices[i] - minPrice;
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+        return maxProfit;
+    }
+}
